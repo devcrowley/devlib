db02905ed87ed3a33afd005681b4bbe9ab2c38f8
Devin Crowley <dcrowley@woodrodgers.com>
2020-10-22 12:08:28 -0700

    Extended modules are now classes

--- src/devlib.urltools.js
+++ src/devlib.urltools.js
@@ -1,19 +1,26 @@
-const urlTools = {};
+/**
+ * devlib.urltools.js
+ * Tools for reading and manipulating values from the current URL
+ */
 
 /** Reads value pairs from an URL GET statement
  * For Example, parsing `http://myurl.html?value=yes&happy=youknowit`
  *
  *  to get all values after '?'
  * */
-urlTools.getUrlDataValues = function() {
-    if (window.location.href.search(/\?/g) === -1) return false;
-    const gets = window.location.href.split("?")[1].split("&");
-    const values = {};
-    gets.forEach(g => {
-        const _get = g.split("=");
-        if (_get[1]) values[_get[0]] = _get[1];
-    });
-    return values;
+class UrlTools {
+    getUrlDataValues() {
+        if (window.location.href.search(/\?/g) === -1) return false;
+        const gets = window.location.href.split("?")[1].split("&");
+        const values = {};
+        gets.forEach(g => {
+            const _get = g.split("=");
+            if (_get[1]) values[_get[0]] = _get[1];
+        });
+        return values;
+    }
 }
+
+export const urlTools = new UrlTools();
 
 export default urlTools;
